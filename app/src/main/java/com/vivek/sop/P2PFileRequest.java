6d162042c790224989d96db698a5114b42cafb84
viveksinghvi <viveksinghvi97@gmail.com>
2017-07-20 17:38:49 +0530

    Project Files Uploaded

---
+++ app/src/main/java/com/vivek/sop/P2PFileRequest.java
@@ -0,0 +1,13 @@
+package com.vivek.sop;
+
+public class P2PFileRequest {
+	
+	String requestFile;
+	String requestSource;
+	
+	public P2PFileRequest(String reqfile, String source){
+		requestFile = reqfile;
+		requestSource = source;
+	}
+
+}
